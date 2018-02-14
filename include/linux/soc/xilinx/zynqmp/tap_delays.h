eb26751139977114d9b85c6a08b212e2bcf3b267
Rajan Vaja <rajan.vaja@xilinx.com>
2018-02-14 05:16:59 -0800

    zynqmp: Use firmware IOCTL for device control
    Use IOCTL to control devices instead of MMIO read/write.
    
    Signed-off-by: Rajan Vaja <rajanv@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/linux/soc/xilinx/zynqmp/tap_delays.h
+++ include/linux/soc/xilinx/zynqmp/tap_delays.h
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <linux/soc/xilinx/zynqmp/firmware.h>
+#include <linux/firmware/xilinx/zynqmp/firmware.h>
 
 #ifdef CONFIG_SOC_XILINX_ZYNQMP
 /* API for programming the tap delays */
