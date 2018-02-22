6dc0cc785c4a4342b30dd2b3d8f894f3b6bbb013
Michal Simek <michal.simek@xilinx.com>
2018-02-22 15:39:01 +0100

    soc: xilinx: vcu: Sync driver with mainline
    This driver was upstream without exported symbols.
    Sync this mainline driver with internal one.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/soc/xilinx/xlnx_vcu.h
+++ include/soc/xilinx/xlnx_vcu.h
@@ -1,19 +1,18 @@
 /*
  * Xilinx VCU Init
  *
- * Copyright (C) 2016-2017 Xilinx, Inc.
+ * Copyright (C) 2016 - 2017 Xilinx, Inc.
  *
  * Contacts   Dhaval Shah <dshah@xilinx.com>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * SPDX-License-Identifier: GPL-2.0
  */
 #ifndef _XILINX_VCU_H_
 #define _XILINX_VCU_H_
 
 struct xvcu_device;
 
+u32 xvcu_get_color_depth(struct xvcu_device *xvcu);
 u32 xvcu_get_memory_depth(struct xvcu_device *xvcu);
 
 #endif  /* _XILINX_VCU_H_ */
