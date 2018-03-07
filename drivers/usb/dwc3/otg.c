90b8f86781c6ada15107f5d2b8ca85baabe10903
Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>
2018-03-07 17:02:17 +0530

    usb: dwc3: otg: don't clear event buffers when changing to Host mode from Device mode
    Clearing event buffers when changing from Device mode -> Host mode
    may require allocating event buffers again during role swap from
    Host mode -> Device mode. Changing the event buffers at runtime are
    not triggering events in device mode and also the role swap can happen
    at any time. So, better not to clear the event buffers during role
    swap instead reuse the previously allocated event buffers.
    This patch does the same.
    
    Signed-off-by: Anurag Kumar Vulisha <anuragku@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/usb/dwc3/otg.c
+++ drivers/usb/dwc3/otg.c
@@ -297,7 +297,6 @@ static int start_host(struct dwc3_otg *otg)
 	u32 octl;
 	u32 osts;
 	u32 dctl;
-	u32 event_addr;
 	struct usb_hcd *hcd;
 	struct xhci_hcd *xhci;
 
@@ -311,12 +310,6 @@ static int start_host(struct dwc3_otg *otg)
 		otg_dbg(otg, "Disabling the RUN/STOP bit\n");
 		dctl &= ~DWC3_DCTL_RUN_STOP;
 		otg_write(otg, DCTL, dctl);
-	}
-
-	event_addr = dwc3_readl(otg->dwc->regs, DWC3_GEVNTADRLO(0));
-	if (event_addr != 0x0) {
-		otg_dbg(otg, "Freeing the device event buffers\n");
-		dwc3_free_event_buffers(otg->dwc);
 	}
 
 	if (!set_peri_mode(otg, PERI_MODE_HOST)) {
@@ -336,7 +329,6 @@ static int start_host(struct dwc3_otg *otg)
 	/* Start host driver */
 
 	*(struct xhci_hcd **)hcd->hcd_priv = xhci;
-	otg_dbg(otg, "1- calling usb_add_hcd() irq=%d\n", otg->hcd_irq);
 	ret = usb_add_hcd(hcd, otg->hcd_irq, IRQF_SHARED);
 	if (ret) {
 		otg_err(otg, "%s: failed to start primary hcd, ret=%d\n",
@@ -346,7 +338,6 @@ static int start_host(struct dwc3_otg *otg)
 
 	*(struct xhci_hcd **)xhci->shared_hcd->hcd_priv = xhci;
 	if (xhci->shared_hcd) {
-		otg_dbg(otg, "2- calling usb_add_hcd() irq=%d\n", otg->hcd_irq);
 		ret = usb_add_hcd(xhci->shared_hcd, otg->hcd_irq, IRQF_SHARED);
 		if (ret) {
 			otg_err(otg,
@@ -489,15 +480,7 @@ static void host_release(struct dwc3_otg *otg)
 static void dwc3_otg_setup_event_buffers(struct dwc3_otg *otg)
 {
 	if (dwc3_readl(otg->dwc->regs, DWC3_GEVNTADRLO(0)) == 0x0) {
-		int ret;
 
-		otg_dbg(otg, "allocating the event buffer\n");
-		ret = dwc3_alloc_event_buffers(otg->dwc,
-				DWC3_EVENT_BUFFERS_SIZE);
-		if (ret) {
-			dev_err(otg->dwc->dev,
-					"failed to allocate event buffers\n");
-		}
 		otg_dbg(otg, "setting up event buffers\n");
 		dwc3_event_buffers_setup(otg->dwc);
 	}
